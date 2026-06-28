6f6c2ec2301ae19c14284df1853b50afc1cc565a
mjsalimin <mjsn800403@outlook.com>
2026-06-28 15:44:34 +0330

    Add STATIC_ROOT for production collectstatic

--- KG_backend/KG_backend/settings.py
+++ KG_backend/KG_backend/settings.py
@@ -146,6 +146,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Per-car image assets (svg/png/etc referenced from manual HTML content).
 # Each car gets a folder named exactly after its db file, e.g.
